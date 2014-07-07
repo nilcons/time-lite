f4589bd9a853e161197bb2098ef947d06e7f32be
Mihaly Barasz <klao@nilcons.com>
2014-07-07 22:17:28 +0200

    Add thyme to the benchmark

--- benchmarks/gettimeofday_bench.hs
+++ benchmarks/gettimeofday_bench.hs
@@ -1,17 +1,25 @@
 module Main (main) where
 
+import Control.Applicative
 import Control.Lens
 import Criterion.Main
 import Data.Time.Lite
 import Data.Time.Lite.Nano
 import Data.Time
 import Data.Time.Clock.POSIX
+import qualified Data.Thyme as Th
+import qualified Data.Thyme.Time as Th
 
 benchmarks :: [Benchmark]
 benchmarks = [
+  -- Data.Time
   bench "getCurrentTime" $ nfIO $ getCurrentTime,
-  bench "getCurrentTime'" $ nfIO $ getCurrentTime',
   bench "getPOSIXTime" $ nfIO $ getPOSIXTime,
+  -- Data.Thyme
+  bench "Thyme.getCurrentTime" $ whnfIO $ Th.getCurrentTime,
+  bench "Thyme.getCurrentTime compat" $ nfIO $ getCurrentTimeThyme,
+  -- Ours
+  bench "getCurrentTime'" $ nfIO $ getCurrentTime',
   bench "getCurrentUTime" $ whnfIO $ getCurrentUTime,
   bench "getClockUTime REALTIME" $ whnfIO $ getClockUTime CLOCK_REALTIME,
   bench "getClockUTime REALTIME_COARSE" $ whnfIO $ getClockUTime CLOCK_REALTIME_COARSE
@@ -22,6 +30,9 @@ getCurrentTime' = do
   UTime ns <- getCurrentUTime
   let (d, dt) = ns `iQuotRem` 86400
   return $ UTCTime (ModifiedJulianDay $ 40587 + d) (dt ^. nanoDiffTime)
+
+getCurrentTimeThyme :: IO UTCTime
+getCurrentTimeThyme = Th.fromThyme <$> Th.getCurrentTime
 
 main :: IO ()
 main = do
